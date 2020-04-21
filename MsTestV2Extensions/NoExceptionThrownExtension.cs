4607e870ee33e221f09f8f1daf2a7eb553303e15
Adam Stráník <stranik.adam@gmail.com>
2020-04-21 14:56:12 +0200

    Add NoExceptionThrown extension

---
+++ MsTestV2Extensions/NoExceptionThrownExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MsTestV2Extensions
+{
+    /// <summary>
+    /// Defines the extension NoExceptionThrown
+    /// </summary>
+    public static class NoExceptionThrownExtension
+    {
+        /// <summary>
+        /// Performs no action, just letting know the intention: no exception is expected in this test.
+        /// </summary>
+        public static void NoExceptionThrown(this Assert assert)
+        {
+            // nothing to do, it's just a syntactic sugar
+        }
+    }
+}
